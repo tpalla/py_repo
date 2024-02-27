e04ad75da48ea23358ec1828b2eb3a91024e8c9d
tpalla <tpallapothu@arigs.com>
2024-02-27 14:15:55 -0600

    changes made by emp2

--- test.py
+++ test.py
@@ -6,4 +6,14 @@ for i in range(0, 5):
         print(a)
 print("2) This is a file edited by emp1 branch")
 b = 5
-print(a+b)
+print("value in emp1",a+b)
+print("3) Changes made in emp2 branch")
+a = 0
+b = 5
+c = a+b
+for i in range(0, 5):
+    a = a+i
+    if a > 0:
+        print(a)
+        print("Inside the if loop:", c)
+    print("outside if loop:", c)
